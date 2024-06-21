05a975f17cfe0cee6636355de15ca80fa6e65d32
fozery <fozery@yandex.ru>
2024-06-21 20:19:21 +0300

    init: init project, add package.json, node_modules,eslint + prettier

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const globals = require('globals')
+const pluginJs = require('@eslint/js')
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
+
+
+module.exports = [
+    {
+        files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' },
+    },
+    { languageOptions: { globals: { ...globals.node, ...globals.es2021 } } },
+    pluginJs.configs.recommended,
+    eslintPluginPrettierRecommended,
+]
