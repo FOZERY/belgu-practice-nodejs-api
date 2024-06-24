8e6fd4db8b2c2084c48cd95ede8203ad4357192f
fozery <fozery@yandex.ru>
2024-06-24 21:12:34 +0300

    fix: remove extra fields in getCourseByTeacherId query

--- src/models/course.model.js
+++ src/models/course.model.js
@@ -3,7 +3,7 @@ const db = require('../../database/db')
 class CourseModel {
     async getCoursesByTeacherId(id) {
         const query =
-            'SELECT * FROM course AS c LEFT JOIN teacher_course AS tc ON tc.course_id = c.id WHERE tc.teacher_id = $1'
+            'SELECT id, course_name, course_description FROM course AS c LEFT JOIN teacher_course AS tc ON tc.course_id = c.id WHERE tc.teacher_id = $1'
         const { rows } = await db.query(query, [id])
         return rows
     }
