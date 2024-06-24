ae05dd7d48788e49841143da608ca67529ca078c
fozery <fozery@yandex.ru>
2024-06-24 19:25:24 +0300

    fix: remove throw error in lesson model

--- src/models/lesson.model.js
+++ src/models/lesson.model.js
@@ -2,7 +2,6 @@ const db = require('../../db')
 
 class LessonModel {
     async getLessonsByTeacherId(id) {
-        throw new Error('error')
         const query = 'SELECT * FROM lesson WHERE teacher_id = $1'
         const { rows } = await db.query(query, [id])
         return rows
