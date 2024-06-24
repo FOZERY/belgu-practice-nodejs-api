db6826055dbc1f54468376e529de2c66eee8e169
fozery <fozery@yandex.ru>
2024-06-24 21:05:22 +0300

    fix: fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,9 @@ module.exports = [
                 {
                     'usePrettierrc': true,
                 },
+                {
+                    "endOfLine": auto
+                }
             ],
         },
     },
