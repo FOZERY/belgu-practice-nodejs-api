17395048e650c155bb751b822d04492b7e35b459
fozery <fozery@yandex.ru>
2024-06-25 13:23:51 +0300

    feat: group groups for each lesson in teacher.service

--- src/services/teacher.service.js
+++ src/services/teacher.service.js
@@ -10,6 +10,14 @@ class TeacherService {
 
     async getTeacherLessons(teacher_id) {
         const lessons = await LessonModel.getLessonsByTeacherId(teacher_id)
+
+        for (let i = 0, length = lessons.length; i < length; i++) {
+            const lesson = lessons[i]
+            if (lesson.groups.length === 1 && !lesson.groups.group_id) {
+                lesson.groups = []
+            }
+        }
+
         return lessons
     }
 }
