4ac8eb8779b17c49548c4e49c20f07a7df254b65
fozery <fozery@yandex.ru>
2024-06-28 20:35:45 +0300

    feat: add new methods in ApiError

--- src/error/ApiError.js
+++ src/error/ApiError.js
@@ -6,7 +6,15 @@ class ApiError extends Error {
     }
 
     static badRequest(message) {
+        return new ApiError(400, message)
+    }
+
+    static notFound(message) {
         return new ApiError(404, message)
+    }
+
+    static unauthorized(message) {
+        return new ApiError(401, message)
     }
 
     static internal(message) {
