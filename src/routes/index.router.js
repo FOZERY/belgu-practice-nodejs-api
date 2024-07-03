be9f4f99a82523281dc993e0e59a1e983f44ddb0
fozery <fozery@yandex.ru>
2024-07-03 21:27:17 +0300

    feat : add grade router and post method for adding grades

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -6,11 +6,13 @@ const teacherRouter = require('./teacher.router')
 const courseRouter = require('./course.router')
 const studentRouter = require('./student.router')
 const groupRouter = require('./group.router')
+const gradeRouter = require('./grade.router')
 
 router.use('/user', userRouter)
 router.use('/teacher', teacherRouter)
 router.use('/course', courseRouter)
 router.use('/student', studentRouter)
 router.use('/group', groupRouter)
+router.use('/grade', gradeRouter)
 
 module.exports = router
